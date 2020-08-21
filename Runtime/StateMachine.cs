ecce81b5c7e0dcb321a2de3181498156ce24262f
JeffK <jeff.kwak@gmail.com>
2020-08-21 19:55:46 -0400

    Initial commit adds Permit, Configure, and Fire for basic statemachine
    The project is developed as an embedded package. Publishing the release
    to GitHub will allow for the package to be shared.

---
+++ Runtime/StateMachine.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gr8tGames
+{
+  public class StateMachine<TState, TTrigger>
+  {
+    public TState State { get; private set; }
+
+    private IDictionary<TState, IDictionary<TTrigger, TState>> transitions;
+    private IDictionary<TState, Action> onEnter;
+    private IDictionary<TState, Action> onExit;
+
+    public StateMachine(TState initialState)
+    {
+      State = initialState;
+      transitions = new Dictionary<TState, IDictionary<TTrigger, TState>>();
+      onEnter = new Dictionary<TState, Action>();
+      onExit = new Dictionary<TState, Action>();
+    }
+
+    public Configuration Configure(TState state)
+    {
+      return new Configuration(this, state);
+    }
+
+    public void Fire(TTrigger trigger)
+    {
+      if (!transitions.ContainsKey(State)) return;
+      if (!transitions[State].ContainsKey(trigger)) return;
+
+      if(onExit.ContainsKey(State))
+      {
+        onExit[State]();
+      }
+      
+      State = transitions[State][trigger];
+
+      if(onEnter.ContainsKey(State))
+      {
+        onEnter[State]();
+      }
+    }
+
+    public class Configuration
+    {
+      private readonly StateMachine<TState, TTrigger> target;
+      private readonly TState forState;
+
+      public Configuration(StateMachine<TState, TTrigger> target, TState forState)
+      {
+        this.target = target;
+        this.forState = forState;
+      }
+
+      public Configuration Permit(TTrigger trigger, TState nextState)
+      {
+        if (!target.transitions.ContainsKey(forState))
+        {
+          target.transitions[forState] = new Dictionary<TTrigger, TState>();
+        }
+
+        target.transitions[forState][trigger] = nextState;
+
+        return this;
+      }
+
+      public Configuration OnEnter(Action action)
+      {
+        target.onEnter[forState] = action;
+        return this;
+      }
+
+      public Configuration OnExit(Action action)
+      {
+        target.onExit[forState] = action;
+        return this;
+      }
+    }
+  }
+}
---
+++ Tests/Editor/StateMachineTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+
+namespace Gr8tGames.Tests
+{
+
+  public class StateMachineTests
+  {
+    public enum State
+    {
+      OnHook,
+      Ringing,
+      Connected,
+      OffHook
+    }
+
+    public enum Trigger
+    {
+      PickedUp,
+      CallDialed,
+      CallConnected,
+      HungUp
+    }
+
+    private StateMachine<State, Trigger> machine;
+
+    [Test]
+    public void TheInitialStateIsSetInTheConstructor()
+    {
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+      Assert.That(machine.State, Is.EqualTo(State.OnHook));
+
+      machine = new StateMachine<State, Trigger>(State.OffHook);
+      Assert.That(machine.State, Is.EqualTo(State.OffHook));
+    }
+
+    [Test]
+    public void FiringPermittedTriggerInConfiguredStateTransitionsToNextState()
+    {
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+      machine.Configure(State.OnHook)
+        .Permit(Trigger.PickedUp, State.OffHook);
+
+      machine.Fire(Trigger.PickedUp);
+
+      Assert.That(machine.State, Is.EqualTo(State.OffHook));
+    }
+
+    [Test]
+    public void FiringAnUnpermittedTriggerInConfiguredStateHasNoEffect()
+    {
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+      machine.Configure(State.OnHook)
+        .Permit(Trigger.PickedUp, State.OffHook);
+
+      machine.Fire(Trigger.CallDialed);
+
+      Assert.That(machine.State, Is.EqualTo(State.OnHook));
+    }
+
+    [Test]
+    public void FiringTriggerInUnconfiguredStateHasNoEffect()
+    {
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+
+      machine.Fire(Trigger.PickedUp);
+
+      Assert.That(machine.State, Is.EqualTo(State.OnHook));
+    }
+
+    [Test]
+    public void FiringPermittedTriggerInConfiguredStateCallsOnEnterOfNextState()
+    {
+      bool wasCalled = false;
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+      machine.Configure(State.OnHook)
+        .Permit(Trigger.PickedUp, State.OffHook);
+
+      machine.Configure(State.OffHook)
+        .OnEnter(() => wasCalled = true);
+
+      machine.Fire(Trigger.PickedUp);
+
+      Assert.That(wasCalled, Is.True);
+    }
+
+    [Test]
+    public void FiringPermittedTriggerInConfiguredStateCallsOnExitOfPreviousStateBeforeCallingOnEnterOfNextState()
+    {
+      int sum = 2;
+      machine = new StateMachine<State, Trigger>(State.OnHook);
+      machine.Configure(State.OnHook)
+        .Permit(Trigger.PickedUp, State.OffHook)
+        .OnExit(() => sum += 2);
+
+      machine.Configure(State.OffHook)
+        .OnEnter(() => sum /= 2);
+
+      machine.Fire(Trigger.PickedUp);
+
+      Assert.That(sum, Is.EqualTo(2));
+    }
+  }
+}
